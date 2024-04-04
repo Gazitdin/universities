975552645878b532c89f25fbffe10b17684188de
Arthur Gazitdinov <gazitdin@yandex.ru>
2024-04-04 21:28:20 +0500

    Добавлена функция по сбору ссылок на университеты в get.universities.py

---
+++ get_universities.py
@@ -0,0 +1,32 @@
+#! /bin/python3
+"""this module contains a function for getting a list of links to the pages of
+federal districts on the website of the GIVC."""
+
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_universities(regions_list, prefix):
+    """Function collectштп links to universities"""
+    universities_list = []
+    for item in regions_list:
+        link = item['link']
+        page = requests.get(link, timeout=10)
+        soup = BeautifulSoup(page.text, "lxml")
+        table = soup.find("table", attrs={"class": "an"})
+        rows = table.find_all("td", attrs={"class": "inst"})
+        for row in rows:
+            tmp = row.find("a")
+            link_tmp = tmp["href"]
+            university_id = re.search(r'id=\d+', link_tmp)
+            university_id = re.sub('id=', '', university_id.group(0))
+            university_name = tmp.text
+            university_link = prefix + tmp["href"]
+            university = {
+                'university_id': university_id,
+                'link': university_link,
+                'university_name' : university_name
+                }
+            universities_list.append(university)
+    return universities_list
