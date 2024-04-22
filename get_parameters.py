b1b996f811119da25f6c1fab8423246960cebf27
Gazitdin <gazitdin@yandex.ru>
2024-04-23 00:33:52 +0500

    Завершена функция сбора параметров по вузу

--- get_parameters.py
+++ get_parameters.py
@@ -3,77 +3,71 @@
 univerisy from the pages of university on the website of the GIVC.
 """
 
-import re
 import requests
 from bs4 import BeautifulSoup
 
 
 def get_info(link):
-   """Function to collect base information about unoversity"""
-   page = requests.get(link)
-   soup = BeautifulSoup(page.text, "lxml")
-   info_table = soup.find("table", attrs={"id":"info"})
-   region = info_table.find_all("a")[0].text
-   type_check = info_table.find_all("td", attrs={"colspan": "2"})
-   type = 'Головная организация' if not type_check else 'Филиал'
-   affiliation_table = info_table.find_all("tr")
-   for item in affiliation_table:
-      temp = item.find_all("td")
-      if temp[0].text == 'Ведомственная принадлежность':
-         affiliation = temp[1].text
-   University_info = {
-      "region":region,
-      "type":type,
-      "affiliation":affiliation
-      }
-   return University_info
+    """Function to collect base information about unoversity"""
+    page = requests.get(link, timeout=10)
+    soup = BeautifulSoup(page.text, "lxml")
+    info_table = soup.find("table", attrs={"id":"info"})
+    region = info_table.find_all("a")[0].text
+    type_check = info_table.find_all("td", attrs={"colspan": "2"})
+    university_type = 'Головная организация' if not type_check else 'Филиал'
+    affiliation_table = info_table.find_all("tr")
+    for item in affiliation_table:
+        temp = item.find_all("td")
+        if temp[0].text == 'Ведомственная принадлежность':
+            affiliation = temp[1].text
+    university_info = {
+       "region":region,
+       "type":university_type,
+       "affiliation":affiliation
+       }
+    return university_info
 
 
 def get_parameters (link):
-   """Function to collect parameters of universities performance from GIVC"""
-   page = requests.get(link)
-   soup = BeautifulSoup(page.text, "lxml")
-   indicator_tables = soup.find_all("table", attrs={"class":"napde"})
-   content = []
-   for item in indicator_tables:
-      rows_list = item.find_all("tr", attrs={"class":"",})
-      for row in rows_list:
-         cells_list = row.find_all("td")
-         indicator_number = cells_list[0].text
-         indicator_name = cells_list[1].text
-         uom = cells_list[2].text
-         indicator_value = cells_list[3].text
-         indicator_row = {"indicator_number": indicator_number,
-                          "indicator_name": indicator_name,
-                          "uom": uom,
-                          "indicator_value": indicator_value}
-         content.append(indicator_row)
-   addition_table = soup.find_all("table", attrs={"id":"analis_dop"})
-   
-   return content
-
-TEST_LINK = 'https://monitoring.miccedu.ru/iam/2023/_vpo/inst.php?id=2183'
-print(get_parameters(TEST_LINK))
-
-"""
-
-indicator_tables = soup.find_all("table", attrs={"class":"napde"})
-table_1 = indicator_tables[1]
-
-table_1 = table_1.find_all("tr", attrs={"class":""})
-row1 = table_1[0]
-list = row1.find_all("td")
-row_to_result=list[0].text
-table_of_content = []
-for item in list:
-   content = item.text
-   table_of_content.append(content)
-print(table_of_content)
-
-
-
-
-
-table_of_content = []
-for item in 
-"""
+    """Function to collect parameters of universities performance from GIVC"""
+    page = requests.get(link, timeout=10)
+    soup = BeautifulSoup(page.text, "lxml")
+    indicator_tables = soup.find_all("table", attrs={"class":"napde"})
+    content = []
+    for item in indicator_tables:
+        rows_list = item.find_all("tr", attrs={"class":"",})
+        for row in rows_list:
+            cells_list = row.find_all("td")
+            indicator_number = cells_list[0].text
+            indicator_name = cells_list[1].text
+            uom = cells_list[2].text
+            indicator_value = cells_list[3].text
+            indicator_row = {"indicator_number": indicator_number,
+                           "indicator_name": indicator_name,
+                           "uom": uom,
+                           "indicator_value": indicator_value}
+            content.append(indicator_row)
+    addition_table = soup.find("table", attrs={"id":"analis_dop"})
+    addition_table.thead.clear()
+    rows_list = addition_table.find_all("tr")
+    i = 0
+    for row in rows_list:
+        if len(row) != 1:
+            cells = row.find_all("td")
+            if len(cells) == 4:
+                indicator_number = cells[0].text
+                indicator_name = cells[1].text
+                uom = cells[2].text
+                indicator_value = cells[3].text
+            else:
+                i += 1
+                indicator_number = "1.1." + str(i)
+                indicator_name = cells[0].text
+                uom = cells[1].text
+                indicator_value = cells[2].text
+                indicator_row = {"indicator_number": indicator_number,
+                                 "indicator_name": indicator_name,
+                                 "uom": uom,
+                                 "indicator_value": indicator_value}
+            content.append(indicator_row)
+    return content
